6ca5d49ad4c3b3334292033c8e3174c476232c3f
robertzhao2002 <rzhao1234@gmail.com>
2021-04-17 13:44:53 -0400

    first

---
+++ generate_plot.py
@@ -0,0 +1,150 @@
+import numpy as np
+import triangle_utils as tri
+
+import matplotlib.pyplot as plt
+from matplotlib.patches import Circle, RegularPolygon
+from matplotlib.path import Path
+from matplotlib.projections.polar import PolarAxes
+from matplotlib.projections import register_projection
+from matplotlib.spines import Spine
+from matplotlib.transforms import Affine2D
+
+def radar_factory(num_vars, frame='circle'):
+    """
+    Create a radar chart with `num_vars` axes.
+
+    This function creates a RadarAxes projection and registers it.
+
+    Parameters
+    ----------
+    num_vars : int
+        Number of variables for radar chart.
+    frame : {'circle', 'polygon'}
+        Shape of frame surrounding axes.
+
+    """
+    # calculate evenly-spaced axis angles
+    theta = np.linspace(0, 2*np.pi, num_vars, endpoint=False)
+
+    class RadarAxes(PolarAxes):
+
+        name = 'radar'
+        # use 1 line segment to connect specified points
+        RESOLUTION = 1
+
+        def __init__(self, *args, **kwargs):
+            super().__init__(*args, **kwargs)
+            # rotate plot such that the first axis is at the top
+            self.set_theta_zero_location('N')
+
+        def fill(self, *args, closed=True, **kwargs):
+            """Override fill so that line is closed by default"""
+            return super().fill(closed=closed, *args, **kwargs)
+
+        def plot(self, *args, **kwargs):
+            """Override plot so that line is closed by default"""
+            lines = super().plot(*args, **kwargs)
+            for line in lines:
+                self._close_line(line)
+
+        def _close_line(self, line):
+            x, y = line.get_data()
+            # FIXME: markers at x[0], y[0] get doubled-up
+            if x[0] != x[-1]:
+                x = np.append(x, x[0])
+                y = np.append(y, y[0])
+                line.set_data(x, y)
+
+        def set_varlabels(self, labels):
+            self.set_thetagrids(np.degrees(theta), labels)
+
+        def _gen_axes_patch(self):
+            # The Axes patch must be centered at (0.5, 0.5) and of radius 0.5
+            # in axes coordinates.
+            if frame == 'circle':
+                return Circle((0.5, 0.5), 0.5)
+            elif frame == 'polygon':
+                return RegularPolygon((0.5, 0.5), num_vars,
+                                      radius=.5, edgecolor="k")
+            else:
+                raise ValueError("Unknown value for 'frame': %s" % frame)
+
+        def _gen_axes_spines(self):
+            if frame == 'circle':
+                return super()._gen_axes_spines()
+            elif frame == 'polygon':
+                # spine_type must be 'left'/'right'/'top'/'bottom'/'circle'.
+                spine = Spine(axes=self,
+                              spine_type='circle',
+                              path=Path.unit_regular_polygon(num_vars))
+                # unit_regular_polygon gives a polygon of radius 1 centered at
+                # (0, 0) but we want a polygon of radius 0.5 centered at (0.5,
+                # 0.5) in axes coordinates.
+                spine.set_transform(Affine2D().scale(.5).translate(.5, .5)
+                                    + self.transAxes)
+                return {'polar': spine}
+            else:
+                raise ValueError("Unknown value for 'frame': %s" % frame)
+
+    register_projection(RadarAxes)
+    return theta
+
+
+def example_data():
+    data = [
+        ['BAD', 'CUTE', 'HOT'],
+        ('Girl A', [
+            [0.25, 0.60, 0.99]]),
+        ('Girl B', [
+            [0.99, 0.45, 0.50]]),
+        ('Girl C', [
+            [0.35, 0.99, 0.65]]),
+        ('Girl D', [
+            [0.99, 0.99, 0.99]])
+        # ('?', [
+        #     [0.40, 0.99, 0.75]]),
+        # ('??', [
+        #     [0.99, 0.30, 0.95]]),
+        # ('???', [
+        #     [0.75, 0.25, 0.95]])
+    ]
+    return data
+
+if __name__ == '__main__':
+    N = 3
+    theta = radar_factory(N, frame='circle')
+
+    data = example_data()
+    spoke_labels = data.pop(0)
+
+    for i in range(0, len(data)):
+        list_temp = list(data[i])
+        side_a = tri.sideLength(data[i][1][0][0], data[i][1][0][1])
+        side_b = tri.sideLength(data[i][1][0][2], data[i][1][0][1])
+        side_c = tri.sideLength(data[i][1][0][0], data[i][1][0][2])
+        area = round(tri.triArea(side_a, side_b, side_c), 3)
+        list_temp[0]=str(data[i][0]) + ' [B, C, H]: ' + str(data[i][1][0]) + '\nArea: ' + str(area) + ', PERFECT: ' + str(round(tri.PERFECT_AREA, 3))
+        data[i]=tuple(list_temp)
+
+    fig, axs = plt.subplots(figsize=(9, 9), nrows=2, ncols=2,
+                            subplot_kw=dict(projection='radar'))
+    fig.subplots_adjust(wspace=0.25, hspace=0.20, top=0.85, bottom=0.05)
+
+    colors = ['b']
+    # Plot the four cases from the example data on separate axes
+    for ax, (title, case_data) in zip(axs.flat, data):
+        ax.set_title(title, weight='bold', size='medium', position=(0.5, 1.1),
+                     horizontalalignment='center', verticalalignment='center')
+        for d, color in zip(case_data, colors):
+            ax.plot(theta, d, color=color)
+            ax.fill(theta, d, facecolor=color, alpha=0.25)
+            ax.set_rgrids([0.2, 0.4, 0.6, 0.8])
+        ax.set_varlabels(spoke_labels)
+
+    # add legend relative to top-left plot
+
+    fig.text(0.5, 0.965, 'The BAD-CUTE-HOT (BCH) Female Scale',
+             horizontalalignment='center', color='black', weight='bold',
+             size='large')
+
+    plt.show()
---
+++ triangle_utils.py
@@ -0,0 +1,18 @@
+import math
+
+#Calculates the area of a triangle with side lengths a, b, and c.
+#PRECONDITION: triangle ABC is a valid triangle.
+def triArea(a,b,c):
+    s = (a+b+c)/2
+    area = math.sqrt(s*(s-a)*(s-b)*(s-c))
+    return area
+    
+#Calculates the sidelength of the radar shape based on the 2 other sides
+def sideLength(r1, r2):
+    side_squared = r1**2 + r2**2 - 2*r1*r2*math.cos(2*math.pi/3)
+    return math.sqrt(side_squared)
+
+PERFECT_A=1
+PERFECT_B=1
+PERFECT_C=1
+PERFECT_AREA=triArea(sideLength(PERFECT_A,PERFECT_B), sideLength(PERFECT_C,PERFECT_B), sideLength(PERFECT_A,PERFECT_C))
